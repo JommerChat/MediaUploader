3ef5ba9a472c998058b95bb7416c5f7e50dd7f49
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-11-23 12:35:08 -0500

    Add configuration property to determine target file path

--- src/main/java/com/parlantos/MediaUploader/controller/MediaUploaderController.java
+++ src/main/java/com/parlantos/MediaUploader/controller/MediaUploaderController.java
@@ -3,6 +3,7 @@ package com.parlantos.MediaUploader.controller;
 import org.apache.tika.Tika;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,6 +24,7 @@ public class MediaUploaderController {
 
     private final Logger logger = LoggerFactory.getLogger(MediaUploaderController.class);
 
+    @Value("${images.path:/var/www/images}")
     private final Path root = Paths.get("/var/www/images");
 
     @PostMapping("/upload")
